30a810f4a7b18d56325b388c2024d3b39f7bd59e
Sonic4999 <sonictbm@gmail.com>
2020-07-01 22:08:17 -0400

    Minor fix to a converter

--- cogs/starboard/cmds/star_norm_cmds.py
+++ cogs/starboard/cmds/star_norm_cmds.py
@@ -107,7 +107,7 @@ class StarNormCMDs(commands.Cog, name = "Normal Star"):
 
     @commands.cooldown(1, 5, commands.BucketType.member)
     @commands.command(aliases = ["position", "place", "placing"])
-    async def pos(self, ctx, user_mention: typing.Optional(discord.Member)):
+    async def pos(self, ctx, user_mention: typing.Optional[discord.Member]):
         """Allows you to get either your or whoever you mentioned’s position in the star leaderboard (like the top command, but only for one person)."""
 
         member = ctx.author if user_mention == None else user_mention
