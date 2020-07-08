fa853d8a7da70537ed6bcba7e45cd281c35a5207
Sonic4999 <sonictbm@gmail.com>
2020-07-08 14:50:52 -0400

    Allow people to see what options for starboard are set to

--- cogs/core/settings/star_settings.py
+++ cogs/core/settings/star_settings.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-import discord
+import discord, typing
 
 import common.utils as utils
 
@@ -13,33 +13,45 @@ async def main_cmd(ctx):
 
 @main_cmd.command()
 @commands.check(utils.proper_permissions)
-async def channel(ctx, channel: discord.TextChannel):
-    """Sets the starboard channel to the channel mentioned."""
-    ctx.bot.config[ctx.guild.id]["starboard_id"] = channel.id
-    await ctx.send(f"Set channel to {channel.mention}!")
+async def channel(ctx, channel: typing.Optional[discord.TextChannel]):
+    """Allows you to either get the starboard channel (no argument) or set the starboard channel (with argument)."""
+
+    if channel != None:
+        ctx.bot.config[ctx.guild.id]["starboard_id"] = channel.id
+        await ctx.send(f"Set channel to {channel.mention}!")
+    else:
+        starboard_id = ctx.bot.config[ctx.guild.id]['starboard_id']
+        starboard_mention = f"<#{starboard_id}>" if starboard_id != None else "None"
+        await ctx.send(f"Starboard channel: {starboard_mention}")
 
 @main_cmd.command()
 @commands.check(utils.proper_permissions)
-async def limit(ctx, limit: int):
-    """Sets the amount of stars needed to get onto the starboard to that number."""
-    if limit.isdigit():
-        ctx.bot.config[ctx.guild.id]["star_limit"] = int(limit)
-        await ctx.send(f"Set limit to {limit}!")
+async def limit(ctx, limit: typing.Optional[int]):
+    """Allows you to either get the amount of stars needed to get on the starboard (no argument) or set the amount (with argument)."""
+    if limit != None:
+        if limit.isdigit():
+            ctx.bot.config[ctx.guild.id]["star_limit"] = int(limit)
+            await ctx.send(f"Set limit to {limit}!")
+        else:
+            await ctx.send("That doesn't seem like a valid number to me...")
     else:
-        await ctx.send("That doesn't seem like a valid number to me...")
+        await ctx.send(f"Star limit: {ctx.bot.config[ctx.guild.id]['star_limit']}")
 
 @main_cmd.command()
 @commands.check(utils.proper_permissions)
-async def toggle(ctx, toggle: bool):
-    """Toggles the entirety of the starboard (including the commands, hopefully) on or off (defaults to off when it joins a new server).
-    To toggle it, both the star channel and the star limit must be set beforehand."""
+async def toggle(ctx, toggle: typing.Optional[bool]):
+    """Allows you to either see if all starboard-related commands and actions are on or off (no argument) or allows you to toggle that (with argument).
+    If you wish to set the toggle, both the starboard channel and the star limit must be set first."""
 
-    guild_config = ctx.bot.config[ctx.guild.id]
-    if guild_config["starboard_id"] != None and guild_config["star_limit"] != None:
-        ctx.bot.config[ctx.guild.id]["star_toggle"] = toggle
-        await ctx.send(f"Toggled starboard to {toggle} for this server!")
+    if toggle != None:
+        guild_config = ctx.bot.config[ctx.guild.id]
+        if guild_config["starboard_id"] != None and guild_config["star_limit"] != None:
+            ctx.bot.config[ctx.guild.id]["star_toggle"] = toggle
+            await ctx.send(f"Toggled starboard to {toggle} for this server!")
+        else:
+            await ctx.send("Either you forgot to set the starboard channel or the star limit. Please try again.")
     else:
-        await ctx.send("Either you forgot to set the starboard channel or the star limit. Please try again.")
+        await ctx.send(f"Star toggle: {ctx.bot.config[ctx.guild.id]['star_toggle']}")
 
 
 def star_toggle_check(ctx):
