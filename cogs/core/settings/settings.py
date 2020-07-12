9b7cb35ba6c62e6cbee5d0dab38834ad6f373919
Sonic4999 <sonictbm@gmail.com>
2020-07-12 15:33:47 -0400

    Quick name fix

--- cogs/core/settings/settings.py
+++ cogs/core/settings/settings.py
@@ -25,7 +25,7 @@ class Settings(commands.Cog, name="Settings"):
         if settings_cmd == None:
             raise commands.CommandNotFound("Can't find settings command!")
 
-        settings_ext = utils.find_all_extensions(os.environ.get("DIRECTORY_OF_FILE"), "cogs/core/settings")
+        settings_ext = utils.get_all_extensions(os.environ.get("DIRECTORY_OF_FILE"), "cogs/core/settings")
         for setting in settings_ext:
             if setting == "cogs.core.settings.settings":
                 continue
