2e7af11aa5c5e741a9e787bb3626f052157cebe7
Sonic4999 <sonictbm@gmail.com>
2020-07-21 12:32:51 -0400

    Minor fix to base_generate

--- common/star_mes_handler.py
+++ common/star_mes_handler.py
@@ -73,8 +73,11 @@ async def base_generate(bot, mes):
         # next pieces of code make the embed more how a normally generated embed would be like
         send_embed.color = discord.Colour(0xcfca76)
         send_embed.timestamp = mes.created_at
-        send_embed.remove_field(0)
         send_embed.set_footer() # will set footer to default, aka none
+
+        for x in range(len(send_embed.fields)):
+            if send_embed.fields[x].name == "Original":
+                send_embed.remove_field(x)
 
     elif mes.embeds != [] and ((mes.author.id in [270904126974590976, 499383056822435840] 
     and mes.embeds[0].author.name != discord.Embed.Empty) or (mes.author.id == bot.user.id 
