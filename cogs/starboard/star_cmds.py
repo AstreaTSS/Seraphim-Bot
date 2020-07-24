595fd921de259e18ce903340e349fb2191088939
Sonic4999 <sonictbm@gmail.com>
2020-07-23 23:44:17 -0400

    Hopefully final changes to optimizations

--- cogs/starboard/star_cmds.py
+++ cogs/starboard/star_cmds.py
@@ -30,10 +30,10 @@ class StarCMDs(commands.Cog, name = "Starboard"):
             return None
 
     def get_user_placing(self, user_star_list, author_id):
-        author_entry = [e for e in user_star_list if e[0] == author_id]
-        if author_entry != ():
-            author_index = user_star_list.index(author_entry[0])
-            return f"position: #{author_index + 1} with {author_entry[0][1]} ⭐"
+        author_entry = discord.utils.find(lambda e: e[0] == author_id, user_star_list)
+        if author_entry != None:
+            author_index = user_star_list.index(author_entry)
+            return f"position: #{author_index + 1} with {author_entry[1]} ⭐"
         else:
             return "position: N/A - no stars found!"
     
