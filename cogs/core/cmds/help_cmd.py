29587755eab8020a218e67d5d64546057caf7e77
Sonic4999 <sonictbm@gmail.com>
2020-08-17 14:44:28 -0400

    Minor fix to help command

--- cogs/core/cmds/help_cmd.py
+++ cogs/core/cmds/help_cmd.py
@@ -31,7 +31,7 @@ class HelpPaginator(paginator.Pages):
         self.embed.set_footer(text=f'Use "{self.prefix}help command" for more info on a command.')
 
         for entry in entries:
-            signature = f'{entry.qualified_name} {entry.signature}'
+            signature = f'{entry.qualified_name.replace("_", "-")} {entry.signature}'
             self.embed.add_field(name=signature, value=entry.short_doc or "No help given", inline=False)
 
         if self.maximum_pages:
