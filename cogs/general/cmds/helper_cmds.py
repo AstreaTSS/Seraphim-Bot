aefec9a10cbb93c6cf3452599af77a6e1321e126
Sonic4999 <sonictbm@gmail.com>
2020-09-22 19:07:30 -0400

    It's always the awaits

--- cogs/general/cmds/helper_cmds.py
+++ cogs/general/cmds/helper_cmds.py
@@ -42,9 +42,8 @@ class HelperCMDs(commands.Cog, name = "Helper"):
                 added_roles.append(role)
 
         if added_roles:
-            await utils.msg_to_owner(self.bot, added_roles)
             try:
-                member.add_roles(added_roles, reason=f"Restoring old roles: done by {str(ctx.author)}.")
+                await member.add_roles(added_roles, reason=f"Restoring old roles: done by {str(ctx.author)}.")
             except discord.HTTPException as error:
                 await ctx.send("Something happened while trying to restore the roles this user had.\n" +
                 "This shouldn't be happening, and this should have been caught earlier by the bot. Try contacting the bot owner about it.\n" +
