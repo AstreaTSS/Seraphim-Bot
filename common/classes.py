8fdda9f29e2ed08bf4fabdb65c2b3abe6f59b752
Sonic4999 <sonictbm@gmail.com>
2021-01-22 09:11:10 -0500

    Rewrote setup cmd to use classes

--- common/classes.py
+++ common/classes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3.7
 from discord.ext import commands
-import discord, re, datetime, asyncio
+import discord, re, datetime
+import asyncio, typing
 
 class SnipedMessage():
     """A special class for sniped messages, using slots to keep the memory usage to a minimum."""
@@ -99,3 +100,68 @@ class TimeDurationConverter(commands.Converter):
             raise commands.BadArgument(f"Argument {argument} is not a valid time duration.")
 
         return datetime.timedelta(seconds=time_span)
+
+class WizardManager():
+    """A class that allows you to make a wizard of sorts, allowing a more intuitive way of getting multiple inputs from a user."""
+
+    def __init__(self, embed_title: str, timeout: float, final_text: str, color = discord.Colour(0x4378fc)):
+        self.embed_title = embed_title
+        self.timeout = timeout
+        self.final_text = final_text
+        self.color = color
+
+        self.questions = []
+        self.ori_mes = typing.Optional[discord.Message]
+
+    def add_question(self, question: str, converter: function, action: function):
+        self.questions.append((question, converter, action))
+
+    async def run(self, ctx: commands.Context):
+        def check(m):
+            return m.author == ctx.author and m.channel == ctx.channel
+
+        wizard_embed = discord.Embed(
+            title = self.embed_title, 
+            colour = self.color
+        )
+        wizard_embed.set_author(
+            name=f"{ctx.bot.user.name}", 
+            icon_url=f"{str(ctx.guild.me.avatar_url_as(format=None,static_format='png', size=128))}"
+        )
+        wizard_embed.set_footer(text="If you wish to stop this setup at any time, just type in 'exit'.")
+
+        for question in self.questions:
+            wizard_embed.description = question[0]
+
+            if not self.ori_mes:
+                self.ori_mes = await ctx.reply(embed=wizard_embed)
+            else:
+                await self.ori_mes.edit(embed=wizard_embed)
+
+            try:
+                reply = await ctx.bot.wait_for('message', check=check, timeout=self.timeout)
+            except asyncio.TimeoutError:
+                wizard_embed.description = "Failed to reply. Exiting..."
+                wizard_embed.set_footer(text=discord.Embed.Empty)
+                await self.ori_mes.edit(embed=wizard_embed)
+                return
+            else:
+                if reply.content.lower() == "exit":
+                    wizard_embed.description = "Exiting..."
+                    wizard_embed.set_footer(text=discord.Embed.Empty)
+                    await self.ori_mes.edit(embed=wizard_embed)
+                    return
+
+            try:
+                converted = await discord.utils.maybe_coroutine(question[1], ctx, reply.content)
+            except:
+                wizard_embed.description = "Invalid input. Exiting..."
+                wizard_embed.set_footer(text=discord.Embed.Empty)
+                await self.ori_mes.edit(embed=wizard_embed)
+                return
+
+            await discord.utils.maybe_coroutine(question[2], ctx, converted)
+
+        wizard_embed.description = self.final_text
+        wizard_embed.set_footer(text=discord.Embed.Empty)
+        await self.ori_mes.edit(embed=wizard_embed)
