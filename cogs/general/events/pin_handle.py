ac68bf43048ef726a0927f6609d0d0ff532082ff
Sonic4999 <sonictbm@gmail.com>
2021-08-15 21:54:12 -0400

    ???????????????????????

--- cogs/general/events/pin_handle.py
+++ cogs/general/events/pin_handle.py
@@ -24,7 +24,7 @@ class PinHandler(commands.Cog):
             return
 
         pins = await msg.channel.pins()
-        await utils.msg_to_owner(self.bot, pins)
+        await utils.msg_to_owner(self.bot, chan_entry["reversed"])
 
         if len(pins) > chan_entry["limit"]:
 
