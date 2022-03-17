efce402dc3315e2a3505860954dbd5d7db9c49aa
Sonic4999 <sonictbm@gmail.com>
2022-03-17 14:17:17 -0400

    refactor: polish up owner_cmds and fix bugs

--- cogs/core/cmds/owner_cmds.py
+++ cogs/core/cmds/owner_cmds.py
@@ -5,7 +5,6 @@ from enum import Enum
 
 import discord
 from discord.ext import commands
-from discord.types.command import ApplicationCommandOption
 
 import common.paginator as paginator
 import common.star_classes as star_classes
@@ -19,7 +18,9 @@ class OwnerCMDs(commands.Cog, name="Owner", command_attrs=dict(hidden=True)):
     async def cog_check(self, ctx):
         return await self.bot.is_owner(ctx.author)
 
-    @commands.command(hidden=True, aliases=["reloadallextensions"])
+    @commands.command(
+        hidden=True, aliases=["reloadallextensions", "reload_all", "reloadall"]
+    )
     async def reload_all_extensions(self, ctx):
         extensions = [i for i in self.bot.extensions.keys() if i != "cogs.db_handler"]
         for extension in extensions:
@@ -33,18 +34,6 @@ class OwnerCMDs(commands.Cog, name="Owner", command_attrs=dict(hidden=True)):
         exten_str = ", ".join(exten_list)
         await ctx.reply(f"Extensions: {exten_str}")
 
-    class OptionTypeEnum(Enum):
-        SUB_COMMAND = 1
-        SUB_COMMAND_GROUP = 2
-        STRING = 3
-        INTEGER = 4
-        BOOLEAN = 5
-        USER = 6
-        CHANNEL = 7
-        ROLE = 8
-        MENTIONABLE = 9
-        NUMBER = 10
-
     @commands.command(hidden=True, aliases=["list_application_commands", "listappcmds"])
     async def list_app_cmds(
         self, ctx: utils.SeraContextBase, guild: typing.Optional[discord.Guild]
@@ -56,48 +45,47 @@ class OwnerCMDs(commands.Cog, name="Owner", command_attrs=dict(hidden=True)):
                 ctx.bot.application_id, guild.id
             )
 
-        slash_entries = []
+        app_cmd_entries = []
 
         if not app_cmds:
             raise commands.BadArgument(
-                "This guild/bot does not have any specific slash commands."
+                "This guild/bot does not have any specific application commands."
             )
 
         for entry in app_cmds:
-            if entry.get("type", 0) == 1:
-                entry_str_list = []
+            entry_str_list = []
 
-                if entry["description"]:
-                    entry_str_list.append(entry["description"])
-                else:
+            if entry["description"]:
+                entry_str_list.append(entry["description"])
+            else:
+                cmd_type = entry.get("type")
+                if not cmd_type or cmd_type == 1:
                     entry_str_list.append("No description provided.")
+                elif cmd_type == 2:
+                    entry_str_list.append("A user context menu command.")
+                elif cmd_type == 3:
+                    entry_str_list.append("A message context menu command.")
 
-                if entry.get("options"):
-                    entry_str_list.append("__Arguments:__")
+            if options := entry.get("options"):
+                entry_str_list.append("__Arguments:__")
 
-                    for option in entry["options"]:  # type: ignore
-                        option: ApplicationCommandOption
-                        option_type = self.OptionTypeEnum(option["type"]).name
-                        required_txt = ", required" if option.get("required") else ""
-                        entry_str_list.append(
-                            f"{option['name']} (type {option_type}{required_txt}) -"
-                            f" {option['description']}"
-                        )
+                for option in options:
+                    option_type = discord.AppCommandOptionType(option["type"]).name
+                    required_txt = ", required" if option.get("required") else ""
+                    entry_str_list.append(
+                        f"{option['name']} (type {option_type}{required_txt}) -"
+                        f" {option['description']}"
+                    )
 
-                slash_entries.append(
-                    (f"{entry['name']} - ID {entry['id']}", "\n".join(entry_str_list))
-                )
-
-        if not slash_entries:
-            raise commands.BadArgument(
-                "This guild/bot does not have any specific slash commands."
+            app_cmd_entries.append(
+                (f"{entry['name']} - ID {entry['id']}", "\n".join(entry_str_list))
             )
 
-        pages = paginator.FieldPages(ctx, entries=slash_entries, per_page=6)
+        pages = paginator.FieldPages(ctx, entries=app_cmd_entries, per_page=6)
         await pages.paginate()
 
-    @commands.command(hidden=True, aliases=["removeslashcmd"])
-    async def remove_slash_cmd(
+    @commands.command(hidden=True, aliases=["removeappcmd"])
+    async def remove_app_cmd(
         self,
         ctx,
         cmd: discord.Object,
@@ -112,26 +100,15 @@ class OwnerCMDs(commands.Cog, name="Owner", command_attrs=dict(hidden=True)):
 
         await ctx.reply("Removed command.")
 
-    @commands.command(hidden=True, aliases=["removeallslashcmds"])
-    async def remove_all_slash_cmds(self, ctx, guild: typing.Optional[discord.Guild]):
+    @commands.command(hidden=True, aliases=["removeallappcmds"])
+    async def remove_all_app_cmds(self, ctx, guild: typing.Optional[discord.Guild]):
         if not guild:
-            app_cmds = await self.bot.http.get_global_commands(self.bot.application_id)
+            # this is actually a bulk overwrite, but okay d.py
+            await self.bot.http.bulk_upsert_global_commands(self.bot.application_id, [])
         else:
-            app_cmds = await self.bot.http.get_guild_commands(
-                self.bot.application_id, guild.id
+            await self.bot.http.bulk_upsert_guild_commands(
+                self.bot.application_id, guild.id, []
             )
-
-        slash_cmd_ids = [e["id"] for e in app_cmds if e.get("type", 0) == 1]
-
-        for cmd_id in slash_cmd_ids:
-            if not guild:
-                await self.bot.http.delete_global_command(
-                    self.bot.application_id, cmd_id
-                )
-            else:
-                await self.bot.http.delete_guild_command(
-                    self.bot.application_id, guild.id, cmd_id
-                )
 
         await ctx.reply("Removed all commands.")
 
